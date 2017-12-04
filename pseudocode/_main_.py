76b447d455ffd8127ebb941315538f7f429f5873
josueisonfire <josueisonfire@gmail.com>
2017-12-05 00:44:20 +0900

    Added all new framework files
    hahah!
    Added test for gui file.
    Added file to build the main GUI on.
    Some prunning, and comments in the provided code.

---
+++ pseudocode/_main_.py
@@ -0,0 +1,11 @@
+# pseudocode for the main prgram to run in the demo.
+# must be able to do the following:
+#       1. Enroll students (fingerprint): w/a student ID
+#       2. Detect students, and show it on the database.
+#       3. --> on the front end of the systems, it should be able to:
+#       4. show attendance records, and be able to show enrolling students.
+
+#   Tentatively will be using the pycurses library to implement the gui.
+
+#  Import the main library.
+import fingerpi as fp
