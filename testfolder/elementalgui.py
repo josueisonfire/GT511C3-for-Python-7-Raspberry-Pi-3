557afd6db0eaa70b5d71330bd47e6b7314b7dac1
josueisonfire <josueisonfire@gmail.com>
2017-12-05 01:18:51 +0900

    1.2

--- testfolder/elementalgui.py
+++ testfolder/elementalgui.py
@@ -8,14 +8,16 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-print bcolors.WARNING + "Warning: No active frommets remain. Continue?"
-print bcolors.HEADER + "HEADER"
-print bcolors.OKBLUE + "OKBLUE: asdf;lkjasdf;lkjasdf;lkj"
-print bcolors.OKGREEN + "OKGREEN: sadf;lkjasdf;lkkjasdf;lkjasdf;lkj"
-print bcolors.WARNING + "WARNING: sadf;lkjasdf;lkjasdf;lkjasdflkjkj"
-print bcolors.FAIL + "FAIL: sadf;lkjasdf;lkjasdf;lkjasdf;lkjkasdf;lkjk"
-print bcolors.ENDC + "ENDC: I dunno what this hsit does... lol"
-print bcolors.BOLD + "BOLD: Here's some boldness."
-print bcolors.UNDERLINE + "UNDERLINE: Here's and underlines text!"
+# print bcolors.WARNING + "Warning: No active frommets remain. Continue?"
+# print bcolors.HEADER + "HEADER"
+# print bcolors.OKBLUE + "OKBLUE: asdf;lkjasdf;lkjasdf;lkj"
+# print bcolors.OKGREEN + "OKGREEN: sadf;lkjasdf;lkkjasdf;lkjasdf;lkj"
+# print bcolors.WARNING + "WARNING: sadf;lkjasdf;lkjasdf;lkjasdflkjkj"
+# print bcolors.FAIL + "FAIL: sadf;lkjasdf;lkjasdf;lkjasdf;lkjkasdf;lkjk"
+# print bcolors.ENDC + "ENDC: I dunno what this hsit does... lol"
+# print bcolors.BOLD + "BOLD: Here's some boldness."
+# print bcolors.UNDERLINE + "UNDERLINE: Here's and underlines text!"
+
+print bcolors.ENDC "[  " + bcolors.OKGREEN + bcolors.BOLD + "OK" + bcolors.ENDC + "  ]  Succesfully created dank meme." + 
 
 # test some of the system.
