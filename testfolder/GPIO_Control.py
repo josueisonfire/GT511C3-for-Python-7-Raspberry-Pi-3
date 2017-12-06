e5580de3dfee6c7d7d4c5d10d3db10c87f0d0eaa
josueisonfire <josueisonfire@gmail.com>
2017-12-07 00:13:40 +0900

    0.31.6
    Some cleaning

--- testfolder/GPIO_Control.py
+++ testfolder/GPIO_Control.py
@@ -1,33 +1,19 @@
-import RPi.GPIO as GPIO
-
 import time
-
+import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
-
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
 
-GPIO.setup(7,GPIO.OUT)
+def dim():
+    red_led = GPIO.PWM(7, 100)
+    red_led.start(0)
+    pause_time = 0.010
+    for i in range(0, 100+1):
+        red_led.ChangeDutyCycle(i)
+        time.sleep(pause_time)
+    for i in range(100, -1, -1):
+        red_led.ChangeDutyCycle(i)
+        time.sleep(pause_time)
+    GPIO.cleanup()
 
-def dim()
-
-	red_led = GPIO.PWM(7,100)
-
-	red_led.start(0)
-
-	pause_time = 0.010
-
-	for i in range(0,100+1):
-
-		red_led.ChangeDutyCycle(i)
-
-		time.sleep(pause_time)
-
-	for i in range(100,-1,-1):
-
-		red_led.ChangeDutyCycle(i)
-
-		time.sleep(pause_time)
-
-	GPIO.cleanup()
-
-dim()<br>
+dim()
