c98909ce1c7c0372fb6de782a4f0bcdac2e883d7
josueisonfire <josueisonfire@gmail.com>
2017-12-07 00:31:59 +0900

    0.31.7

--- testfolder/elementalgui.py
+++ testfolder/elementalgui.py
@@ -7,7 +7,32 @@ import select
 from threading import Timer
 from exceptions import *
 import fingerpi as fp
-import GPIO_Control
+
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+led = GPIO.PWM(7, 100)
+led.start(0)
+
+# slightly light up LED.
+def dim(pt = 0.001):
+    pause_time = pt
+    for i in range(0, 100+1):
+        led.ChangeDutyCycle(i)
+        time.sleep(pause_time)
+    for i in range(100, -1, -1):
+        led.ChangeDutyCycle(i)
+        time.sleep(pause_time)
+    GPIO.cleanup()
+
+def turnLEDON():
+    GPIO.output(7, True)
+
+def turnLEDOFF():
+    GPIO.output(7, False)
+
 
 # Global variables:
 port = '/dev/ttyAMA0'
