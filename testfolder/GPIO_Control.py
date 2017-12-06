affbc06e257c0dbd647dddad7dba4bfe8f26caa8
josueisonfire <josueisonfire@gmail.com>
2017-12-07 00:10:43 +0900

    0.31.5

--- testfolder/GPIO_Control.py
+++ testfolder/GPIO_Control.py
@@ -1,23 +1,33 @@
-import RPi.GPIO as GPIO ## Import GPIO library
-import time ## Import 'time' library. Allows us to use 'sleep'
+import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+import time
 
-##Define a function named Blink()
-def Blink(numTimes,speed):
-    for i in range(0,numTimes):## Run loop numTimes
-        print "Iteration " + str(i+1)## Print current loop
-        GPIO.output(7,True)## Switch on pin 7
-        time.sleep(speed)## Wait
-        GPIO.output(7,False)## Switch off pin 7
-        time.sleep(speed)## Wait
-        print "Done" ## When loop is complete, print "Done"
-        GPIO.cleanup()
+GPIO.setwarnings(False)
 
-## Ask user for total number of blinks and length of each blink
-iterations = raw_input("Enter total number of times to blink: ")
-speed = raw_input("Enter length of each blink(seconds): ")
+GPIO.setmode(GPIO.BOARD)
 
-## Start Blink() function. Convert user input from strings to numeric data types and pass to Blink() as parameters
-Blink(int(iterations),float(speed))
+GPIO.setup(7,GPIO.OUT)
+
+def dim()
+
+	red_led = GPIO.PWM(7,100)
+
+	red_led.start(0)
+
+	pause_time = 0.010
+
+	for i in range(0,100+1):
+
+		red_led.ChangeDutyCycle(i)
+
+		time.sleep(pause_time)
+
+	for i in range(100,-1,-1):
+
+		red_led.ChangeDutyCycle(i)
+
+		time.sleep(pause_time)
+
+	GPIO.cleanup()
+
+dim()<br>
